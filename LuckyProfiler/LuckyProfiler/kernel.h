9ecff9b9e6afb4b19a604cf3c6e91491f12bbfff
SRMLabHUST <44626749+SRMLabHUST@users.noreply.github.com>
2022-06-07 22:08:28 +0800

    Delete LuckyProfiler directory

--- LuckyProfiler/LuckyProfiler/kernel.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-extern double* angs;
-extern short* RCadi;
-extern long* RelatedLocIndex;
-
-void getAngs(long* locx, long* locy, short* SkelImwithout1, int SkelNum, int imgH, int imgW);
-void getRCadiAndRelatedLocIndex(short* SkelImwithout1, long* SingleResult, long* rr, int PreProjectionNum, int imH, int imgW, int skelNum);
-//void bwlookup(int* bw, int i, int imgH, int imgW);
--- LuckyProfiler/LuckyProfiler/skeleton.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-long *compute_skeleton_gradient(short *img, int nrow, int ncol);
-bool* algbwmorph(bool * bw, int imgH, int imgW);
