ec341a8687d57331a3d2acc7f27b4684fe34489e
SRMLabHUST <44626749+SRMLabHUST@users.noreply.github.com>
2022-06-15 09:24:47 +0800

    Delete LuckyProfiler directory

--- LuckyProfiler/LuckyProfiler/kernel.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-extern double* angs;
-extern long* RelatedLocIndex,* RCadi;
-
-void getAngs(long* locx, long* locy, long* SkelImwithout1, int SkelNum, int imgH, int imgW);
-void getRCadiAndRelatedLocIndex(long* SkelImwithout1, long* SingleResult, long* rr, int PreProjectionNum, int imH, int imgW, int skelNum);
-//void bwlookup(int* bw, int i, int imgH, int imgW);
